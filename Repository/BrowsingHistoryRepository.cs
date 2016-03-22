ca111006efb4e57bb9f9235444aef38e7f238f41
Oliver Slade <oliverslade@users.noreply.github.com>
2016-03-22 13:33:56 +0000

    Browsing History Repo

---
+++ Repository/BrowsingHistoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class BrowsingHistoryRepository
+    {
+        private readonly MeCommerceDbContext _context = new MeCommerceDbContext();
+
+        public IEnumerable<BrowsingHistory> GetUsersBrowsingHistories(int userId)
+        {
+            return _context.BrowsingHistory.Where(x => x.UserId == userId);
+        }
+
+        public void CreateBrowsingHistoryEntry(BrowsingHistory bhe)
+        {
+            _context.BrowsingHistory.Add(bhe);
+            _context.SaveChanges();
+        }
+    }
+}
